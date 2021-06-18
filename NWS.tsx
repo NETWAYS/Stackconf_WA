c4a6dc96c5885adb4b02638b3653fced37d37132
jhartmann44 <jhartmann@netways.de>
2021-06-18 14:49:28 +0200

    add Foreman Event

--- NWS.tsx
+++ NWS.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="NWS" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="NWS.png" width="96" height="64"/>
+<tileset version="1.5" tiledversion="1.6.0" name="NWS" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="NWS.png" width="128" height="64"/>
 </tileset>
